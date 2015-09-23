748911e9dd54c7b17c8edd5e0353e28ebdaccdce
FabienB4 <fabienb4.b4soft@gmail.com>
2015-09-23 17:24:25 -0600

    [BREAKING] 0.2.0

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'fabienb4:materialize-components',
-  version: '0.1.4',
+  version: '0.2.0',
   // Brief, one-line summary of the package.
   summary: 'Easy to use templates for Materialize.',
   // URL to the Git repository containing the source code for this package.
